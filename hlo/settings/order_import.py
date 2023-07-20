8b584fde3d5c87aada8d2236d9bf43dc8bcbe38a
Anders Einar Hilden <hildenae@gmail.com>
2023-07-20 23:11:34 +0200

    import -> input

--- hlo/settings/order_import.py
+++ hlo/settings/order_import.py
@@ -5,10 +5,10 @@ env = environ.FileAwareEnv()
 
 env.prefix = "OI_"
 
-IMPORT_FOLDER: Path = Path(
-        env("IMPORT_FOLDER", default="./import")
+INPUT_FOLDER: Path = Path(
+        env("INPUT_FOLDER", default="./input")
     ).resolve()
 
 JSON_SCHEMA: Path = Path(
-        env("JSON_SCHEMA", default="./import/schema.json")
+        env("JSON_SCHEMA", default="./input/schema.json")
     ).resolve()
