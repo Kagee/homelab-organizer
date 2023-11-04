da83ffd927b894343be3996cb08bc870aeeaee80
Anders Einar Hilden <hildenae@gmail.com>
2023-11-04 22:43:22 +0100

    Play with some tempaltes and icons

--- hlo/urls.py
+++ hlo/urls.py
@@ -3,10 +3,11 @@ from django.conf import settings
 from django.urls import path #, include
 from django.conf.urls.static import static
 from django.contrib import admin
-
+from django.conf.urls import include, url
 
 urlpatterns = [
     #path("", include("order_scraper.urls")),
     #path("orders/", OrderListView.as_view(), name="order-list"),
     path("admin/", admin.site.urls),
+    url('', include('inventory.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    # https://docs.djangoproject.com/en/4.2/ref/templates/builtins/
+    # https://getbootstrap.com/docs/5.0/examples/navbars/
+    # https://thenounproject.com/icon/hacker-1482319/
+    return render(request, template_name="inventory/index.html")
