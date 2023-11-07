6640541f90a14725ce4bdd2cfa48901bd16c4784
Anders Einar Hilden <hildenae@gmail.com>
2023-11-07 16:23:42 +0100

    More tests on OrderItemFilterwq

--- loader/filters.py
+++ loader/filters.py
@@ -9,13 +9,7 @@ class OrderItemFilter(django_filters.FilterSet):
         ('istartswith', 'Starts with'),
         ('iexact', 'Equals'),
     ],
-    #initial=('icontains', '---------'),
-    #initial='icontains',
-    #initial='Contains',
     )
-    # fields = (field, ChoiceField(choices=lookup_choices, empty_label=empty_label))
-    #     widget = LookupChoiceWidget(widgets=[f.widget for f in fields])
-    #name.field.
 
     year = django_filters.AllValuesFilter(field_name='order__date', lookup_expr='year')
     class Meta:
