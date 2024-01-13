c31603840ebdaca8428cce7a714136c2dc80fda5
Anders Einar Hilden <hildenae@gmail.com>
2024-01-13 21:47:09 +0100

    ruff autofix complete

--- hlo/admin/__init__.py
+++ hlo/admin/__init__.py
@@ -1,17 +1,25 @@
 import logging
 
-from django.contrib import admin
-
-from mptt.admin import DraggableMPTTAdmin
-
-from .orderitem import OrderItemAdmin
-from .stockitem import StockItemAdmin
-from .order import OrderAdmin
-from .shop import ShopAdmin
 from .attachement import AttachementAdmin
 from .category import CategoryAdmin
-from .project import ProjectAdmin
-from .storage import StorageAdmin
+from .order import OrderAdmin
+from .orderitem import OrderItemAdmin
 from .orderitemmeta import OrderItemMetaAdmin
+from .project import ProjectAdmin
+from .shop import ShopAdmin
+from .stockitem import StockItemAdmin
+from .storage import StorageAdmin
 
 logger = logging.getLogger(__name__)
+
+__all__ =  [
+    "AttachementAdmin",
+    "CategoryAdmin",
+    "OrderAdmin",
+    "OrderItemAdmin",
+    "OrderItemMetaAdmin",
+    "ProjectAdmin",
+    "ShopAdmin",
+    "StockItemAdmin",
+    "StorageAdmin",
+]
