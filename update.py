1a2a8fa643b4144672ed655387cde0ffa0fe5d5e
Anders Einar Hilden <hildenae@gmail.com>
2024-03-10 22:56:44 +0100

    Restore update db deletion functionallity

--- update.py
+++ update.py
@@ -4,71 +4,132 @@ from bootstrap import python_checks
 python_checks()
 
 # pylint: disable=wrong-import-position,wrong-import-order
+import shutil  # noqa: E402
 import subprocess  # noqa: E402
 import sys  # noqa: E402
+from pathlib import Path  # noqa: E402
 
-nuke = input("Upgrade pip? (Y/n): ")
-if nuke.lower() != "n":
-    subprocess.run(
-        [  # noqa: S603
-            sys.executable,
-            "-m",
-            "pip",
-            "install",
-            "--require-virtualenv",
-            "--no-user",
-            "--upgrade",
-            "pip",
-        ],
-        check=False,
-    )
+from environs import Env  # noqa: E402
 
-nuke = input("Pip install? (Y/n): ")
-if nuke.lower() != "n":
-    subprocess.run(
-        [  # noqa: S603
-            sys.executable,
-            "-m",
-            "pip",
-            "install",
-            "--require-virtualenv",
-            "--no-user",
-            "--upgrade",
-            "-r",
-            "requirements-dev.txt",
-        ],
-        check=False,
-    )
+env = Env()
+with env.prefixed("UPDATE_HLO_"):
+    env.read_env(".env", recurse=False, verbose=True)
+    SUPERUSER_NAME = env("SUPERUSER_NAME")
+    SUPERUSER_EMAIL = env("SUPERUSER_EMAIL")
 
-nuke = input("Init shops? (Y/n): ")
-if nuke.lower() != "n":
-    subprocess.run(
-        [  # noqa: S603
-            sys.executable,
-            "manage.py",
-            "hlo",
-            "--init-shops",
-        ],
-        check=False,
-    )
 
-nuke = input("Init order metadata without attachements? (Y/n): ")
-if nuke.lower() != "n":
-    subprocess.run(
-        [  # noqa: S603
-            sys.executable,
-            "manage.py",
-            "hlo",
-            "--import-shop",
-            "all",
-            "--skip-attachements",
-        ],
-        check=False,
-    )
+def main():
+    if input("Upgrade pip? (Y/n): ").lower() != "n":
+        subprocess.run(
+            [  # noqa: S603
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                "--require-virtualenv",
+                "--no-user",
+                "--upgrade",
+                "pip",
+            ],
+            check=False,
+        )
 
-nuke = input("Init order metadata with attachements ?  (Y/n): ")
-if nuke.lower() != "n":
-    subprocess.run(
-        [sys.executable, "manage.py", "hlo", "--import-shop", "all"],  # noqa: S603
-        check=False,
-    )
+    if input("Pip install && upgrade? (Y/n): ").lower() != "n":
+        subprocess.run(
+            [  # noqa: S603
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                "--require-virtualenv",
+                "--no-user",
+                "--upgrade",
+                "-r",
+                "requirements-dev.txt",
+            ],
+            check=False,
+        )
+
+    db_deleted = False
+    migrations_ran = False
+
+    if input("Delete DB? (y/N): ").lower() == "y":
+        p = Path("db/db-dev.sqlite3")
+        if p.is_file():
+            p.unlink()
+        db_deleted = True
+
+    if input("Delete, recreate and apply migrations? (y/N): ").lower() == "y":
+        shutil.rmtree(Path("hlo/migrations"))
+        subprocess.run(
+            [  # noqa: S603
+                sys.executable,
+                "manage.py",
+                "makemigrations",
+                "hlo",
+            ],
+            check=False,
+        )
+        subprocess.run(
+            [  # noqa: S603
+                sys.executable,
+                "manage.py",
+                "migrate",
+            ],
+            check=False,
+        )
+        migrations_ran = True
+
+    if db_deleted and migrations_ran:
+        print("Database deleted, must recreate superuser.V")  # noqa: T201
+        print(f"Username: {SUPERUSER_NAME}")  # noqa: T201
+        print(f"Email: {SUPERUSER_EMAIL}")  # noqa: T201
+        subprocess.run(
+            [  # noqa: S603
+                sys.executable,
+                "manage.py",
+                "createsuperuser",
+                "--username",
+                SUPERUSER_NAME,
+                "--email",
+                SUPERUSER_EMAIL,
+            ],
+            check=False,
+        )
+
+    if input("Init shops? (Y/n): ").lower() != "n":
+        subprocess.run(
+            [  # noqa: S603
+                sys.executable,
+                "manage.py",
+                "hlo",
+                "--init-shops",
+            ],
+            check=False,
+        )
+
+    if (
+        input("Init order metadata without attachements? (Y/n): ").lower()
+        != "n"
+    ):
+        subprocess.run(
+            [  # noqa: S603
+                sys.executable,
+                "manage.py",
+                "hlo",
+                "--import-shop",
+                "all",
+                "--skip-attachements",
+            ],
+            check=False,
+        )
+
+    if input("Init order metadata with attachements ?  (Y/n): ").lower() != "n":
+        subprocess.run(
+            [sys.executable, "manage.py", "hlo", "--import-shop", "all"],  # noqa: S603
+            check=False,
+        )
+
+
+if __name__ == "__main__":
+    main()
