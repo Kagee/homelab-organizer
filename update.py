5459607dbeca81037c260c0d49a958d55ab02ca2
Anders Einar Hilden <hildenae@gmail.com>
2024-04-20 23:23:56 +0200

    update.py: more cli options

--- update.py
+++ update.py
@@ -21,8 +21,7 @@ try:
 except ModuleNotFoundError:
     pass
 
-def main(*, auto_answer: bool, only_input: bool) -> None:
-    print(f"{auto_answer=}, {only_input=}")  # noqa: T201
+def _01_upgrade_pip():
     if not only_input and (
         auto_answer or input("Upgrade pip? (Y/n): ").lower() != "n"
     ):
@@ -37,9 +36,10 @@ def main(*, auto_answer: bool, only_input: bool) -> None:
                 "--upgrade",
                 "pip",
             ],
-            check=False,
+            check=True,
         )
 
+def _02_pip_install_upgrade():
     if not only_input and (
         auto_answer or input("Pip install && upgrade? (Y/n): ").lower() != "n"
     ):
@@ -55,15 +55,16 @@ def main(*, auto_answer: bool, only_input: bool) -> None:
                 "-r",
                 "requirements-dev.txt",
             ],
-            check=False,
+            check=True,
         )
 
     if not environs_loaded:
         print("Restart script to load environs module")
         sys.exit(0)
-    db_deleted = False
-    migrations_ran = False
 
+
+def _03_delete_db():
+    db_deleted = False
     if not only_input and (
         auto_answer or input("Delete DB? (y/N): ").lower() == "y"
     ):
@@ -71,7 +72,12 @@ def main(*, auto_answer: bool, only_input: bool) -> None:
         if p.is_file():
             p.unlink()
         db_deleted = True
+    return db_deleted
 
+def _04_migrations(db_deleted = None):
+    if db_deleted is None:
+        db_deleted = _03_delete_db()
+    migrations_ran = False
     if not only_input and (
         auto_answer
         or input("Delete, recreate and apply migrations? (y/N): ").lower()
@@ -85,7 +91,7 @@ def main(*, auto_answer: bool, only_input: bool) -> None:
                 "makemigrations",
                 "hlo",
             ],
-            check=False,
+            check=True,
         )
         subprocess.run(
             [  # noqa: S603
@@ -93,9 +99,14 @@ def main(*, auto_answer: bool, only_input: bool) -> None:
                 "manage.py",
                 "migrate",
             ],
-            check=False,
+            check=True,
         )
         migrations_ran = True
+    return (db_deleted, migrations_ran)
+
+def _05_superuser(db_deleted =None, migrations_ran = None):
+    if db_deleted is None and migrations_ran is None:
+        db_delete, migrations_ran = _04_migrations()
 
     if db_deleted and migrations_ran:
         print("Database deleted, must recreate superuser.V")  # noqa: T201
@@ -111,9 +122,10 @@ def main(*, auto_answer: bool, only_input: bool) -> None:
                 "--email",
                 SUPERUSER_EMAIL,
             ],
-            check=False,
+            check=True,
         )
 
+def _06_init_shops():
     if only_input or auto_answer or input("Init shops? (Y/n): ").lower() != "n":
         subprocess.run(
             [  # noqa: S603
@@ -122,9 +134,10 @@ def main(*, auto_answer: bool, only_input: bool) -> None:
                 "hlo",
                 "--init-shops",
             ],
-            check=False,
+            check=True,
         )
 
+def _07_order_metadata():
     if (
         only_input
         or auto_answer
@@ -140,17 +153,45 @@ def main(*, auto_answer: bool, only_input: bool) -> None:
                 "all",
                 "--skip-attachements",
             ],
-            check=False,
+            check=True,
         )
 
-    if input("Init order metadata with attachements ?  (Y/n): ").lower() != "n":
+def _08_order_attachements():
+    if (only_input
+        or auto_answer 
+        or input("Init order metadata with attachements ?  (Y/n): ").lower() 
+        != "n"):
         subprocess.run(
             [sys.executable, "manage.py", "hlo", "--import-shop", "all"],  # noqa: S603
-            check=False,
+            check=True,
         )
+
+def main() -> None:
+    print(f"{auto_answer=}, {only_input=}")  # noqa: T201
 
 
 if __name__ == "__main__":
-    auto_answer = "-y" in sys.argv
-    only_input = "-i" in sys.argv
-    main(auto_answer=auto_answer, only_input=only_input)
+    # YES I KNOW; SUE ME!!
+    print_help = "-h" in sys.argv or "--help" in sys.argv
+    auto_answer = "-y" in sys.argv or "--yes" in sys.argv
+    only_input = "-i" in sys.argv or "--import" in sys.argv
+    l = []
+    import inspect
+    import types
+    funcs = inspect.getmembers(sys.modules[__name__], predicate=lambda x: isinstance(x, types.FunctionType) and x.__module__ == __name__ and x.__name__.startswith("_0"))
+    funcs = [(x[4:],y) for (x,y) in funcs]
+    any_func_called = False
+    for fname, func in funcs:
+        if f"--{fname}" in sys.argv:
+            any_func_called = True
+            func()
+
+    if print_help:
+        print("-y\t\tAuto answer yes to every question")
+        print("-i\t\tOnly run nondestructive import functions")
+        print("")
+        print("Call functions directly (may use multiple)")
+        for fname, func in funcs:
+            print(f"\t--{fname}")
+    elif not any_func_called:
+        main()
