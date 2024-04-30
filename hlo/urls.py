c25a5ce10ff2d7dbfbd0840c879e2d087ceafccd
Anders Einar Hilden <hildenae@gmail.com>
2024-04-30 19:54:02 +0200

    Refactor urls.py

--- hlo/urls.py
+++ hlo/urls.py
@@ -16,43 +16,24 @@ from .views import (
 )
 
 urlpatterns = [
-    *[
-        path("admin/", admin.site.urls, name="admin"),
-        # path("admin/", hlo_admin.urls, name="admin"),  # noqa: ERA001
-        # django-debug-toolbar
-        path("__debug__/", include("debug_toolbar.urls")),
-        # django-select2
-        path("select2/", include("django_select2.urls")),
-        # This is used by the tag-selector on stockitem-create
+    *[  # This is where dev happens
         path(
-            "select2/fields/tags.json",
-            TagAutoResponseView.as_view(),
-            name="stockitem-tag-auto-json",
-        ),
-        # Return empty for favicon
-        path(
-            "favicon.ico",
-            RedirectView.as_view(
-                url="/static/images/logo/hlo-cc0-logo-favicon.ico",
-                permanent=True,
-            ),
+            "barcode/render/<int:pk>.<str:format>",
+            views.barcode_render,
+            name="barcode-redirect",
         ),
         path(
-            "",
-            views.index,
-            name="index",
-        ),
-        path("barcode/<str:barcode>", views.barcode, name="barcode"),
-        path(
-            "search/attachements",
-            AttachementSearchView.as_view(),
-            name="attachement-search",
+            "barcode/go/<str:barcode>",
+            views.barcode_redirect,
+            name="barcode-redirect",
         ),
         path(
             "search/items",
             views.item_search,
             name="item-search",
         ),
+    ],
+    *[  # Item stuff
         path("stockitem/list", views.stockitem_list, name="stockitem-list"),
         path(
             "stockitem/detail/<int:pk>",
@@ -80,8 +61,40 @@ urlpatterns = [
             OrderDetailView.as_view(),
             name="order-detail",
         ),
-        # Serve static content through Django
     ],
+    *[  # Stock stuff, not ligely to change
+        path("admin/", admin.site.urls, name="admin"),
+        # path("admin/", hlo_admin.urls, name="admin"),  # noqa: ERA001
+        # django-debug-toolbar
+        path("__debug__/", include("debug_toolbar.urls")),
+        path(
+            "favicon.ico",
+            RedirectView.as_view(
+                url="/static/images/logo/hlo-cc0-logo-favicon.ico",
+                permanent=True,
+            ),
+        ),
+        path(
+            "",
+            views.index,
+            name="index",
+        ),
+        path(
+            "search/attachements",
+            AttachementSearchView.as_view(),
+            name="attachement-search",
+        ),
+    ],
+    *[  # django-select2-stuff
+        path("select2/", include("django_select2.urls")),
+        # This is used by the tag-selector on stockitem-create
+        path(
+            "select2/fields/tags.json",
+            TagAutoResponseView.as_view(),
+            name="stockitem-tag-auto-json",
+        ),
+    ],
+    # Serve static content through Django
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
 
