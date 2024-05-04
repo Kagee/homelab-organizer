02e4915ddd0b70366b568958bd1bb3ee14eaa1f8
Anders Einar Hilden <hildenae@gmail.com>
2024-05-04 21:26:18 +0200

    Preapare for using sha_id on Storage

--- hlo/models/commontree.py
+++ hlo/models/commontree.py
@@ -1,3 +1,4 @@
+import hashlib
 import uuid
 
 from django.db import models
@@ -23,6 +24,12 @@ class CommonTreeModel(MPTTModel):
         editable=False,
     )
 
+    sha1_id = models.CharField(
+        max_length=40,
+        editable=False,
+        blank=True,
+    )
+
     parent = TreeForeignKey(
         "self",
         on_delete=models.CASCADE,
@@ -33,6 +40,12 @@ class CommonTreeModel(MPTTModel):
 
     class Meta:
         abstract = True
+
+    def save(self, *args, **kwargs) -> None:
+        uuid_sha1 = hashlib.sha1()  # noqa: S324
+        uuid_sha1.update(self.uuid).encode()  # defaults to utf-8
+        self.sha1_id = uuid_sha1.hexdigest().upper()
+        super().save(*args, **kwargs)
 
     class MPTTMeta:
         order_insertion_by = ["name"]
