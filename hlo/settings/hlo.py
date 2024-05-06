4035bf3829125a691e88643859f7810438d9cf38
Anders Einar Hilden <hildenae@gmail.com>
2024-05-06 21:28:17 +0200

    openapi settings

--- hlo/settings/hlo.py
+++ hlo/settings/hlo.py
@@ -18,3 +18,18 @@ JSON_SCHEMA: Path = Path(
 BQW_ENDPOINT: str = env("BQW_ENDPOINT", default=None)
 
 QR_URL_PREFIX: str = env("QR_URL_PREFIX", default="")
+
+OPENAPI_PROJECT_API_KEY: str = env("OPENAPI_PROJECT_API_KEY", default="")
+OPENAPI_TITLE_CLEANUP_QUERY: str = env(
+    "OPENAPI_TITLE_CLEANUP_QUERY",
+    default=(
+        "Can you make the title '{title}' shorter and more spesific? "
+        "If possible, mention it's size or dimentions, but do not "
+        "mention quantity, what it is used for, or how it is used."
+    ),
+)
+
+OPENAPI_TITLE_CLEANUP_MODEL: str = env(
+    "OPENAPI_TITLE_CLEANUP_MODEL",
+    default="gpt-4",
+)
