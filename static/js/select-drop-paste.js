daef7409c7b0e0981462710484ccb1e78ffea9e3
Anders Einar Hilden <hildenae@gmail.com>
2024-07-21 22:18:10 +0200

    Move more and more code to class

--- static/js/select-drop-paste.js
+++ static/js/select-drop-paste.js
@@ -7,7 +7,7 @@ class SDP {
            up2u. Default is to print data to console
            based on `debug`.
         */
-        showErrorCallback: SDP._show_error,
+        showErrorCallback: SDP._logError,
         /* function() to hide error message.
            Useful when you i.e. have to
            hide the error message once it
@@ -19,7 +19,7 @@ class SDP {
            Default is to print data to console
            based on `debug`.
         */
-        newFileCallback: SDP._new_file,
+        newFileCallback: SDP._logNewFile,
         /* wether or not (default true) to replace 
            the file in `fileInputElement` when a new
            file has been accepted.
@@ -29,20 +29,64 @@ class SDP {
            potentially update based on `autoAssignFileToInput` 
          */
         fileInputElement: "#uploadFile",
-         /* Element to use as drop zone */
+        /* Element to use as drop zone */
         dropzoneElement: "#drop_zone",
+        /* Acceptable mimetypes for files. Will be used 
+           in String.startsWith(), so "image/" will match
+           among others, image/png, image/jpeg and image/svg+xml.
+        */
+           acceptedMimeTypes: ['image/png', 'image/jpeg'],
         /* Wether or not to print debug messages to console.
         */
         debug: false,
     };
 
     constructor(config) {
-      this._config = this.merge_config();
+        this._config = this.mergeConfig(config);
+        window.addEventListener("load",function() {
+            dze = document.querySelector(this._config.dropzoneElement);
+            dze.addEventListener('drop', this._dropEventHandler);
+            dze.addEventListener('dragover', (e) => {e.preventDefault()});
+            document.addEventListener('paste', this._pastEventHandler);
+        });
     }
 
-    merge_config(new_config) {
+    async _dropEventHandler(ev) {
+        // Prevent default behavior (Prevent file from being opened)
+        ev.preventDefault();
+        if (ev.dataTransfer.items) {
+        for (i=0; i < ev.dataTransfer.items.length; i++) {
+            item = ev.dataTransfer.items[i];
+            if (item.kind === "file") {
+                console.log("Drophandler item kind file:")
+                const file = item.getAsFile();
+                if (use_file(file)) {
+                    break;
+                }
+            }
+        }
+        } else {
+            for (i=0; i < ev.dataTransfer.files.length; i++) {
+                file = ev.dataTransfer.files[i];
+                if (use_file(file)) {
+                    break;
+                }
+            }
+        }
+    }
+
+    async _pastEventHandler(e) {
+        e.preventDefault();
+        for (const clipboardItem of e.clipboardData.files) {
+            if (this._isAcceptableMIMEType(clipboardItem.type)) {
+                    this.use_file(clipboardItem)
+            }
+        }
+    }
+    
+    mergeConfig(new_config) {
         new_config = !!new_config ? new_config : {} // if undefined
-        
+
         let tc = typeof(new_config)
         if (tc != "object") {
             console.log(`SDP config was invalid type ${tc}`)
@@ -68,11 +112,22 @@ class SDP {
         return c
     }
 
-    static _show_error(msg) {
+    _isAcceptableMIMEType(str) {
+        for (let mimeType in acceptedMimeTypes) {
+            if (str.startsWith(mimeType)) {
+                this.D && console.log(`Accepted MIME type ${str}`)
+                return true;
+            }
+        }
+        this.D && console.log(`Rejected MIME type ${str}`)
+        return false;
+    }
+
+    static _logError(msg) {
         this.D && console.log(msg)
     }
 
-    static _new_file(file) {
+    static _logNewFile(file) {
         this.D && console.log(`A new file has been accepted: `, file)
     }
 
