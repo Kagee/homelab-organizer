0c8ac03139461f49eae4c4d4e0e330358137b380
Anders Einar Hilden <hildenae@gmail.com>
2025-04-22 18:49:19 +0200

    Dockerfile -> Containerfile

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd ..
-[[ "$1" = "debug" ]] && exec podman build -f docker/Dockerfile . --tag hlo-debug --build-arg=DEBUG_BUILD=true "${@:2}"
-podman build -f docker/Dockerfile . --tag hlo:prod --build-arg=DEBUG_BUILD=false "$@"
+[[ "$1" = "debug" ]] && exec podman build -f docker/Containerfile . --tag hlo-debug --build-arg=DEBUG_BUILD=true "${@:2}"
+podman build -f docker/Containerfile . --tag hlo:prod --build-arg=DEBUG_BUILD=false "$@"
 podman save localhost/hlo:prod | sudo podman load
