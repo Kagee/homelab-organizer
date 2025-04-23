f1c162da4eaa2c120ec2fe648691a22b87f5e50d
Anders Einar Hilden <hildenae@gmail.com>
2025-04-23 06:56:38 +0200

    Make dev the default docker to build/run

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd ..
-[[ "$1" = "debug" ]] && exec podman build -f docker/Containerfile . --tag hlo-debug --build-arg=DEBUG_BUILD=true "${@:2}"
-podman build -f docker/Containerfile . --tag hlo:prod --build-arg=DEBUG_BUILD=false "$@"
-podman save localhost/hlo:prod | sudo podman load
+[[ "$1" = "prod" ]] && \
+  podman build -f docker/Containerfile . --tag hlo:prod --build-arg=DEBUG_BUILD=false "${@:2}" && \
+  podman save localhost/hlo:prod | sudo podman load && exit
+exec podman build -f docker/Containerfile . --tag hlo:dev --build-arg=DEBUG_BUILD=true "${@:2}"
--- docker/run.sh
+++ docker/run.sh
@@ -1,33 +1,23 @@
 #! /bin/bash
 cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd .. # move to base dir
-[[ "$1" = "debug-mount" ]] && figlet DEBUG-RUN && exec podman run \
+[[ "$1" = "mount-src" ]] && figlet DEV-MOUNT-RUN && exec podman run \
   --init \
   --rm \
-  --name hlo-debug \
-  --env HLO_SECRET_KEY=secret \
-  --env HLO_CSRF_TRUSTED_ORIGINS='https://*' \
-  --env HLO_REMOTE_USER_HEADER=REMOTE_USER \
-  --env HLO_DEBUG=true \
-  --env HLO_REMOTE_USER_HEADER=Cf-Access-Authenticated-User-Email \
-  --env APP_SUPERUSER_EMAIL=hildenae@gmail.com \
+  --name hlo-dev \
+  --env-file ./.env-dev \
   --publish 127.0.0.1:8005:8000 \
   --volume "$PWD/docker/docker-entrypoint.sh:/custom-entrypoint.sh" \
   --volume "$PWD/hlo/:/app/hlo/" \
-  localhost/hlo-debug
+  localhost/hlo:dev
 
-[[ "$1" = "debug" ]] && figlet DEBUG-RUN && exec podman run \
+figlet DEBUG-RUN && exec podman run \
   --init \
   --rm \
-  --name hlo-debug \
-  --env HLO_SECRET_KEY=secret \
-  --env HLO_CSRF_TRUSTED_ORIGINS='https://*' \
-  --env HLO_REMOTE_USER_HEADER=REMOTE_USER \
-  --env HLO_DEBUG=true \
-  --env HLO_REMOTE_USER_HEADER=Cf-Access-Authenticated-User-Email \
-  --env APP_SUPERUSER_EMAIL=hildenae@gmail.com \
+  --name hlo-dev \
+  --env-file ./.env-dev \
   --publish 127.0.0.1:8005:8000 \
-  localhost/hlo-debug
+  localhost/hlo:dev
 
 
 # ENV HLO_STATIC_ROOT=/app/static_root
@@ -35,19 +25,19 @@ cd .. # move to base dir
 # ENV HLO_SQLITE3_FILE=/app/db/hlo.sqlite3
 # ENV HLO_WHOOSH_INDEX=/app/whoosh_index
 
-figlet PROD-RUN && sudo podman run \
-  --init \
-  --rm \
-  --name hlo \
-  --env-file=./.env-prod \
-  --env HLO_DEBUG=false \
-  --env HLO_SQLITE3_FILE=/app/db/hlo.sqlite3 \
-  --env HLO_STATIC_ROOT=/app/static_root \
-  --env HLO_MEDIA_ROOT=/app/media_root \
-  --env HLO_WHOOSH_INDEX=/app/whoosh_index \
-  --volume /tank/data/hlo/static-test/:/app/static_root/ \
-  --volume /tank/data/hlo/media-test/:/app/media_root/ \
-  --volume /tank/data/hlo/db-test/:/app/db/ \
-  --volume /tank/data/hlo/whoosh-test/:/app/whoosh_index/ \
-  --publish 127.0.0.1:8000:8000 \
-  localhost/hlo:prod
+#figlet PROD-RUN && sudo podman run \
+#  --init \
+#  --rm \
+#  --name hlo \
+#  --env-file=./.env-prod \
+#  --env HLO_DEBUG=false \
+#  --env HLO_SQLITE3_FILE=/app/db/hlo.sqlite3 \
+#  --env HLO_STATIC_ROOT=/app/static_root \
+#  --env HLO_MEDIA_ROOT=/app/media_root \
+#  --env HLO_WHOOSH_INDEX=/app/whoosh_index \
+#  --volume /tank/data/hlo/static-test/:/app/static_root/ \
+#  --volume /tank/data/hlo/media-test/:/app/media_root/ \
+#  --volume /tank/data/hlo/db-test/:/app/db/ \
+#  --volume /tank/data/hlo/whoosh-test/:/app/whoosh_index/ \
+#  --publish 127.0.0.1:8000:8000 \
+#  localhost/hlo:prod
