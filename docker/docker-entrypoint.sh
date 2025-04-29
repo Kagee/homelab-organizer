709c1dff3b7de0f6771aa7640b44258d6a9790a0
Anders Einar Hilden <hildenae@gmail.com>
2025-04-29 19:42:58 +0200

    Limit permission change to data files. Mount more when running dev. Huge output & log improvements for dev container

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -14,7 +14,7 @@ APP_USERNAME="${APP_USERNAME:-default}"
 APP_GROUP="${APP_GROUP:-${APP_USERNAME}}"
 
 # App folder
-APP_DIR="${APP_DIR:-/app}" 
+APP_DIR="${APP_DIR:-/app}"
 
 # use the specified UID for the user
 APP_UID="${APP_UID:-1000}"
@@ -27,7 +27,10 @@ APP_SUPERUSER_USERNAME="${APP_SUPERUSER_EMAIL:-${APP_SUPERUSER_EMAIL}}"
 
 # Start waitserver
 cd "$APP_DIR"
-gunicorn --pid /app/waitserver.pid waitserver:application -b 0.0.0.0:8000 &
+echo "INFO: Starting temporary waitserver"
+set -x
+gunicorn --pid /app/waitserver.pid waitserver:application -b 0.0.0.0:8000 2>/dev/null 1>&2 &
+{ set +x; } 2>/dev/null
 
 # check to see if group exists; if not, create it
 if grep -q -E "^${APP_GROUP}:" /etc/group > /dev/null 2>&1
@@ -46,7 +49,7 @@ else
       "$( \
         awk -F ':' '{print $1":"$3}' < /etc/group | grep ":${APP_GID}$" | awk -F ":" '{print $1}' \
       )")
-  set +x
+  { set +x; } 2>/dev/null
 fi
 
 
@@ -66,7 +69,7 @@ else
     --create-home \
     --shell /bin/bash \
     "${APP_USERNAME}";
-  set +x
+  { set +x; } 2>/dev/null
 fi
 
 # change ownership of any directories needed to run my app as the proper UID/GID
@@ -74,28 +77,39 @@ echo "INFO: Setting permissions for ${APP_DIR} to ${APP_UID}:${APP_GID}"
 chown "${APP_UID}:${APP_GID}" "${APP_DIR}"
 
 # Cleanup permissions
-#echo "INFO: Cleanup permissions in subdirectories of ${APP_DIR} to ${APP_UID}:${APP_GID}"
-find "${APP_DIR}" -type d -not -perm 755 -exec chmod 755 {} \;
-find "${APP_DIR}" -type f -not -perm 644 -not -path "/app/docker/*" -exec chmod 644 {} \;
-find "${APP_DIR}" -not \( -uid "${APP_UID}" -and -gid "${APP_GID}" \) -not -path "/app/docker/*" -not -path "/app/hlo/*" -exec chown "${APP_UID}:${APP_GID}" {} \;
+echo "INFO: Cleanup permissions in subdirectories of ${APP_DIR} to ${APP_UID}:${APP_GID}"
+FOLDERS=("${HLO_STATIC_ROOT}" "${HLO_MEDIA_ROOT}" "${HLO_WHOOSH_INDEX}" "$(dirname "${HLO_SQLITE3_FILE}")")
 
-chmod 755 "${APP_DIR}/.venv/bin/gunicorn"
+set -x
+find "${FOLDERS[@]}" \
+    -type d -not -perm 755 -exec chmod 755 {} \;
+find "${FOLDERS[@]}" \
+    -type f -not -perm 644  -exec chmod 644 {} \;
+find "${FOLDERS[@]}" \
+    -not \( -uid "${APP_UID}" -and -gid "${APP_GID}" \) \
+    -exec chown "${APP_UID}:${APP_GID}" {} \;
+{ set +x; } 2>/dev/null
 
 # start myapp
 echo "INFO: Running app from ${APP_DIR} as ${APP_USERNAME}:${APP_GROUP} (${APP_UID}:${APP_GID})"
 
 cd "${APP_DIR}"
 # "Activate" venv for gunicorn etc
-export PATH="${APP_DIR}/.venv/bin:$PATH"
-
-# check for wrong permissions in /app/static_root /app/db /app/media_root /app/whoosh_index
-echo "INFO: Running django migrations"
-gosu "${APP_USERNAME}" python3 manage.py migrate
-
-echo "INFO: Collecting static files"
 set -x
-gosu "${APP_USERNAME}" python3 manage.py collectstatic --no-input
-set +x
+export PATH="${APP_DIR}/.venv/bin:$PATH"
+{ set +x; } 2>/dev/null
+
+echo "INFO: Pre-run Dajngo checks"
+set -x
+gosu "${APP_USERNAME}" python3 manage.py check
+{ set +x; } 2>/dev/null
+
+
+echo "INFO: Running django migrations"
+set -x
+gosu "${APP_USERNAME}" python3 manage.py migrate --skip-checks
+{ set +x; } 2>/dev/null
+
 # Whatever build, if we supply a command, run it.
 [[ $# -ne 0 ]] && \
   cat /app/buildinfo && \
@@ -109,23 +123,37 @@ set +x
 $DEBUG_BUILD && {
   # Fake test data
   echo "INFO: Creating fake app data"
-  gosu "${APP_USERNAME}" python3 manage.py setup_test_data;
+  set -x
+  gosu "${APP_USERNAME}" python3 manage.py setup_test_data --skip-checks;
+  { set +x; } 2>/dev/null
 
   # Create superuser
   echo "INFO: Creating superuser $APP_SUPERUSER_USERNAME ($APP_SUPERUSER_EMAIL)"
-  gosu "${APP_USERNAME}" python3 manage.py createsuperuser \
+  set -x
+  gosu "${APP_USERNAME}" python3 manage.py createsuperuser --skip-checks \
   --username "$APP_SUPERUSER_USERNAME" \
   --email "$APP_SUPERUSER_EMAIL" \
-  --noinput;
+  --noinput 2>/dev/null || { set +x; } 2>/dev/null; echo "INFO: User $APP_SUPERUSER_USERNAME already existed";
+  { set +x; } 2>/dev/null
+
+
+  cat /app/buildinfo;
+
+  # This is the absolute last time we can kill waitserver 
+  echo "INFO: Terminating temporary waitserver"
+  set -x
+  kill "$(cat /app/waitserver.pid)";
+  { set +x; } 2>/dev/null
 
   # Exex as APP_USERNAME a development server on port 8000
-  cat /app/buildinfo;
-  echo "INFO: Debug mode with runserver";
-  kill "$(cat /app/waitserver.pid)";
-  exec gosu "${APP_USERNAME}" python3 manage.py runserver 0.0.0.0:8000;
+  echo "INFO: Starting debug mode with runserver";
+  set -x
+  exec gosu "${APP_USERNAME}" python3 manage.py runserver --skip-checks 0.0.0.0:8000;
 }
 # exec as APP_USERNAME a production server on port 8000
 $DEBUG_BUILD || {
+  echo "INFO: Collecting static files"
+  gosu "${APP_USERNAME}" python3 manage.py collectstatic --no-input
   cat /app/buildinfo;
   echo "INFO: Production mode with gunicorn";
   kill "$(cat /app/waitserver.pid)";
--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
-cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" || exit
 cd .. # move to base dir
-[[ "$1" = "mount-src" ]] && figlet DEV-MOUNT-RUN && exec podman run \
+
+[[ "$1" = "mount-tmp" ]] && figlet DEV-MOUNT-TEMP && exec podman run \
   --init \
   --rm \
   --name hlo-dev \
@@ -9,35 +10,29 @@ cd .. # move to base dir
   --publish 127.0.0.1:8005:8000 \
   --volume "$PWD/docker/docker-entrypoint.sh:/custom-entrypoint.sh" \
   --volume "$PWD/hlo/:/app/hlo/" \
+  --volume "$PWD/static/:/app/static/" \
+  --volume "$PWD/node_modules/:/app/node_modules/" \
   localhost/hlo:dev
 
-figlet DEBUG-RUN && exec podman run \
+[[ "$1" = "mount-perm" ]] && figlet -w 120 DEV-MOUNT-PERM && exec podman run \
+  --init \
+  --rm \
+  --name hlo-dev \
+  --env-file ./.env-dev \
+  --publish 127.0.0.1:8005:8000 \
+  --volume "$PWD/docker/docker-entrypoint.sh:/custom-entrypoint.sh" \
+  --volume "$PWD/hlo/:/app/hlo/" \
+  --volume "$PWD/static/:/app/static/" \
+  --volume "$PWD/node_modules/:/app/node_modules/" \
+  --volume /tank/tmp/hlo-dev/media/:/app/media_root/ \
+  --volume /tank/tmp/hlo-dev/db/:/app/db/ \
+  --volume /tank/tmp/hlo-dev/whoosh:/app/whoosh_index/ \
+  localhost/hlo:dev
+
+figlet DEV-TEMP && exec podman run \
   --init \
   --rm \
   --name hlo-dev \
   --env-file ./.env-dev \
   --publish 127.0.0.1:8005:8000 \
   localhost/hlo:dev
-
-
-# ENV HLO_STATIC_ROOT=/app/static_root
-# ENV HLO_MEDIA_ROOT=/app/media_root
-# ENV HLO_SQLITE3_FILE=/app/db/hlo.sqlite3
-# ENV HLO_WHOOSH_INDEX=/app/whoosh_index
-
-#figlet PROD-RUN && sudo podman run \
-#  --init \
-#  --rm \
-#  --name hlo \
-#  --env-file=./.env-prod \
-#  --env HLO_DEBUG=false \
-#  --env HLO_SQLITE3_FILE=/app/db/hlo.sqlite3 \
-#  --env HLO_STATIC_ROOT=/app/static_root \
-#  --env HLO_MEDIA_ROOT=/app/media_root \
-#  --env HLO_WHOOSH_INDEX=/app/whoosh_index \
-#  --volume /tank/data/hlo/static-test/:/app/static_root/ \
-#  --volume /tank/data/hlo/media-test/:/app/media_root/ \
-#  --volume /tank/data/hlo/db-test/:/app/db/ \
-#  --volume /tank/data/hlo/whoosh-test/:/app/whoosh_index/ \
-#  --publish 127.0.0.1:8000:8000 \
-#  localhost/hlo:prod
