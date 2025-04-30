d1bd6c7859c9dfc9e83c56e88a5695c8aba5880d
Anders Einar Hilden <hildenae@gmail.com>
2025-04-30 08:05:41 +0200

    Some progress of OrderFactory

--- hlo/management/commands/setup_test_data.py
+++ hlo/management/commands/setup_test_data.py
@@ -1,19 +1,29 @@
 # setup_test_data.py
+import datetime
+import random
 import sys
 
 import factory.random
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import transaction
+from factory.fuzzy import FuzzyDateTime
+from faker import Faker
 
 from hlo.models import Shop
 
+# Preseed the random generators to a spesific seed
+# to make the test data reproducible
+random.seed(settings.FACTORY_SEED)
 factory.random.reseed_random(settings.FACTORY_SEED)
 
-from hlo.factories import ShopFactory  # noqa: E402
+from hlo.factories import (
+    OrderFactory,  # noqa: E402
+    ShopFactory,  # noqa: E402
+)
 
 NUM_SHOPS = 10
-NUM_ORDERS = 100
+NUM_ORDERS = 40
 NUM_ORDER_ITEMS = 1000
 
 # https://mattsegal.dev/django-factoryboy-dummy-data.html
@@ -40,11 +50,24 @@ class Command(BaseCommand):
             shop = ShopFactory()
             shops.append(shop)
 
-        # Add some users to clubs
-        # for _ in range(NUM_CLUBS):
-        #    club = ClubFactory()
-        #    members = random.choices(people, k=USERS_PER_CLUB)
-        #    club.user.add(*members)
+        shop_index = list(range(len(shops)))
+        weights = [x + 1 * 2 for x in range(len(shops))]
+        random.shuffle(weights)
+        # print(shop_index, weights)
+        shop_index_list = random.choices(  # noqa: S311
+            shop_index,
+            weights=weights,
+            k=NUM_ORDERS,
+        )
+
+        # Faker.date_between_dates(
+        #    date_start=
+        #    date_end=
+        # )
+        orders = []
+        for _i, shop_index in enumerate(shop_index_list):
+            order = OrderFactory.create(shop=shops[shop_index])
+            orders.append(order)
 
         # Create all the threads
         # for _ in range(NUM_THREADS):
