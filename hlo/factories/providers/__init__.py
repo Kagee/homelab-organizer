427014923494490d4b6d15ff524919e96c011e8c
Anders Einar Hilden <hildenae@gmail.com>
2025-05-03 00:00:50 +0200

    test: TemplateProvider for Faker

--- hlo/factories/providers/__init__.py
+++ hlo/factories/providers/__init__.py
@@ -1,7 +1,9 @@
 from .moneyprovider import MoneyProvider
 from .storageprovider import StorageProvider
+from .templateprovider import TemplateProvider
 
 __all__ = [
     "MoneyProvider",
     "StorageProvider",
+    "TemplateProvider",
 ]
