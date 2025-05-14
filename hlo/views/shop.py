653568fcdd4cc72d6653c4422ab5064a398bca43
Anders Einar Hilden <hildenae@gmail.com>
2025-05-14 21:56:40 +0200

    view: shop: Remove permission checks for now

--- hlo/views/shop.py
+++ hlo/views/shop.py
@@ -3,37 +3,24 @@ import logging
 from crispy_forms.layout import (
     Submit,
 )
-from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
-from django.shortcuts import render
 
 from hlo.forms import ShopForm
 
-# pylint: disable=wildcard-import,unused-wildcard-import
 from hlo.models import Order, Shop
 
 logger = logging.getLogger(__name__)
 
 
-class ShopListView(PermissionRequiredMixin, ListView):
+class ShopListView(ListView):
     model = Shop
     template_name = "shop/list.html"
     context_object_name = "shops"
-    permission_required = ["hlo.view_shop"]
 
-    #def handle_no_permission(self):
-    #    logger.debug("no permission")
-    #    return render(self.request, "common/no_access.html")
-
-    #def get_permission_denied_message(self):
-    #    return "Fuck off"
-
-
-class ShopDetailView(PermissionRequiredMixin, DetailView):
+class ShopDetailView(DetailView):
     model = Shop
     template_name = "shop/detail.html"
     context_object_name = "shop"
-    permission_required = ["hlo.view_shop"]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -41,12 +28,11 @@ class ShopDetailView(PermissionRequiredMixin, DetailView):
         return context
 
 
-class ShopCreateView(PermissionRequiredMixin, CreateView):
+class ShopCreateView(CreateView):
     model = Shop
     template_name = "shop/form.html"
     context_object_name = "shop"
     form_class = ShopForm
-    permission_required = ["hlo.add_shop"]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -59,12 +45,11 @@ class ShopCreateView(PermissionRequiredMixin, CreateView):
         return form
 
 
-class ShopUpdateView(PermissionRequiredMixin, UpdateView):
+class ShopUpdateView(UpdateView):
     model = Shop
     template_name = "shop/form.html"
     context_object_name = "shop"
     form_class = ShopForm
-    permission_required = ["hlo.change_shop"]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
